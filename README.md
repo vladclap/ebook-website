bbfa3e7afbee43b350e6a5a44996c4936098ef05
PolinaFaustova <polinamusikerin@gmail.com>
2023-07-28 20:19:26 +0200

    new-changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ebook Website
 
-Bootstrap website for downloading a free E-book. This website has a light, business-like design.
+Bootstrap website for downloading a free E-book. This website has a light, business-oriented design.
 
 <img src="./images/screen.png"  />
 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
-// Add custom JavaScript here
-
 function userScroll() {
-  const navbar = document.querySelector(".navbar");
+  const navbar = document.querySelector('.navbar');
 
-  window.addEventListener("scroll", () => {
+  window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-      navbar.classList.add("bg-dark");
-      navbar.classList.add("navbar-sticky");
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('navbar-sticky');
     } else {
-      navbar.classList.remove("bg-dark");
-      navbar.classList.remove("navbar-sticky");
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('navbar-sticky');
     }
   });
 }
 
-document.addEventListener("DOMContentLoaded", userScroll);
+document.addEventListener('DOMContentLoaded', userScroll);
